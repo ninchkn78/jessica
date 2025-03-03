d670532dc7a746e0a4d247313123c350a8b5e2c5
Jessica Chen <jc939@duke.edu>
2025-03-03 13:45:25 -0500

    try this

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,10 +2,10 @@
 import { Element } from "react-scroll";
 
 import NavBar from "@/components/NavBar";
-import About from "@/components/sections/About";
-import Teaching from "@/components/sections/Teaching";
-import Service from "@/components/sections/Service";
-import Publications from "@/components/sections/Publications";
+// import About from "@/components/sections/About";
+// import Teaching from "@/components/sections/Teaching";
+// import Service from "@/components/sections/Service";
+// import Publications from "@/components/sections/Publications";
 
 export default function Home() {
   return (
@@ -17,28 +17,32 @@ export default function Home() {
           id="about"
           className="min-h-fit flex flex-col px-12 snap-start snap-always"
         >
-          <About />
+          About
+          {/* <About /> */}
         </Element>
         <Element
           name="teaching"
           id="teaching"
           className="min-h-fit flex flex-col snap-start snap-always"
         >
-          <Teaching />
+          Teaching
+          {/* <Teaching /> */}
         </Element>
         <Element
           name="service"
           id="service"
           className="flex flex-col pt-12 snap-start snap-always"
         >
-          <Service />
+          Service
+          {/* <Service /> */}
         </Element>
         <Element
           name="publications"
           id="publications"
           className="min-h-fit flex flex-col px-12 snap-start snap-always"
         >
-          <Publications />
+          Publications
+          {/* <Publications /> */}
         </Element>
       </div>
     </div>
